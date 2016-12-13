bd836e894cf98aad2cfd15cf7318298edab42a7a
Ethan Bennett <ebennett19@gmail.com>
2016-12-13 16:21:46 -0700

    Add first feature test

---
+++ spec/features/user_creates_an_artist_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "User submits a new artist" do
+  scenario "they enter data to create a new artist" do
+    artist_name       = "The Bad Plus"
+    artist_image_path = "http://www.comusic.org/wp-content/uploads/2016/06/TheBadPlus_OnSacredGround_9135rev-1320953207.jpg"
+
+    visit '/artists'
+    click_on "New Artist"
+    fill_in "artist_name", with: artist_name
+    fill_in "artist_image_path", with: artist_image_path
+    click_on "Create Artist"
+
+    expect(page).to have_content artist_name
+    expect(page).to have_css("img[src=\"#{artist_image_path}\"]")
+  end
+end
