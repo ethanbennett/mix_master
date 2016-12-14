1c46e944f0afd57800807c6cfac391599e88ddc8
Ethan Bennett <ebennett19@gmail.com>
2016-12-13 17:04:16 -0700

    Implement error messages, tests passing

--- spec/features/user_creates_an_artist_spec.rb
+++ spec/features/user_creates_an_artist_spec.rb
@@ -14,4 +14,17 @@ RSpec.feature "User submits a new artist" do
     expect(page).to have_content artist_name
     expect(page).to have_css("img[src=\"#{artist_image_path}\"]")
   end
+
+  context "the submitted data is invalid" do
+    scenario "they see an error message" do
+      artist_image_path = "http://www.comusic.org/wp-content/uploads/2016/06/TheBadPlus_OnSacredGround_9135rev-1320953207.jpg"
+      
+      visit artists_path 
+      click_on "New Artist"
+      fill_in "artist_image_path", with: artist_image_path
+      click_on "Create Artist"
+
+      expect(page).to have_content "Name can't be blank"
+    end
+  end
 end
