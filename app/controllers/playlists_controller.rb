3a743de88c4ffa270e9aaae7f11a062141ddf8a7
Ethan Bennett <ebennett19@gmail.com>
2016-12-14 20:15:14 -0700

    Add Playlist join table

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -4,5 +4,18 @@ class PlaylistsController < ApplicationController
 
   def new
     @playlist = Playlist.new
+    @songs = Song.all
   end
+
+  def create
+    @playlist = Playlist.create(playlist_params)
+    redirect_to @playlist
+  end
+
+private
+
+  def playlist_params
+    params.require(:playlist).permit(:name, :song_ids: [])
+  end
+
 end
