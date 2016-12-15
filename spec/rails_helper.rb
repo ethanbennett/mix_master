4461ac83f866c815c5e4e4d4c5c94285c82e7a36
Ethan Bennett <ebennett19@gmail.com>
2016-12-14 20:01:41 -0700

    Implement first feature test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,6 +16,8 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
