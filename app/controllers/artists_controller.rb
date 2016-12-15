d265266122a7c215609d627d2a62e4ce36b904cc
Ethan Bennett <ebennett19@gmail.com>
2016-12-14 21:14:25 -0700

    Complete refactoring

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,5 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
 
   def index
     @artists = Artist.all
@@ -9,7 +10,7 @@ class ArtistsController < ApplicationController
   end
 
   def create
-    @artist = Artist.create(artist_params)
+    @artist = Artist.new(artist_params)
     if @artist.save
       redirect_to @artist
     else
@@ -18,33 +19,31 @@ class ArtistsController < ApplicationController
   end
 
   def show
-    @artist = Artist.find(params[:id])
   end
 
   def edit
-    @artist = Artist.find(params[:id])
   end
 
   def update
-    @artist = Artist.find(params[:id])
     if @artist.update(artist_params)
-      redirect_to artist_path(@artist)
+      redirect_to @artist
     else
-      @errors = @artist.errors.full_messages
       render :edit
     end
   end
 
   def destroy
-    @artist = Artist.find(params[:id])
-    @artist.delete
+    @artist.destroy
     redirect_to artists_path
   end
 
-  private
+private
 
-  def artist_params
-    params.require(:artist).permit(:name, :image_path)
+  def set_artist
+    @artist = Artist.find(params[:id])  
   end
 
+  def artist_params
+    params.require(:artist).permit(:name, :image_path)  
+  end
 end
