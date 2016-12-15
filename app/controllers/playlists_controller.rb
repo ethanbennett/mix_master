1775b7ff7a7b8a0fe6f1b02797a11e5c3d92fc5f
Ethan Bennett <ebennett19@gmail.com>
2016-12-14 20:18:34 -0700

    Associations set up for Playlist

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -12,10 +12,14 @@ class PlaylistsController < ApplicationController
     redirect_to @playlist
   end
 
+  def show
+    @playlist = Playlist.find(params[:id])
+  end
+
 private
 
   def playlist_params
-    params.require(:playlist).permit(:name, :song_ids: [])
+    params.require(:playlist).permit(:name, song_ids: [])
   end
 
 end
